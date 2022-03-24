fcdf60c3f0d9a9bd6d9355e7871a9c7863378ccd
mohamedhamdy123890456@gmail.com <mohamedhamdy123890456@gmail.com>
2022-03-24 18:53:58 +0200

    /

--- add_field_shipping/models/models.py
+++ add_field_shipping/models/models.py
@@ -8,3 +8,4 @@ class InhStockWarehouse(models.Model):
     employee_ids = fields.Many2many(comodel_name="hr.employee", string="",domain=lambda self: [('department_id', '=', self.env.ref('add_field_shipp.id_creat_data_department').id)])
 
 
+
